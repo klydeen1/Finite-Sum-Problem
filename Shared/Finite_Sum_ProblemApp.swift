1d0d0cd8a9af9f0e6e4932f725c75a8d65d9d40c
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-01 12:29:11 -0600

    Initial Commit

---
+++ Shared/Finite_Sum_ProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finite_Sum_ProblemApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Finite_Sum_ProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
